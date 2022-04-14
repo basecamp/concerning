f1b22ce52eab4b44029064240f71dad918d89701
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-14 22:09:04 +0800

    Delete lib directory

--- lib/concerning/kernel_concern.rb
+++
@@ -1,6 +0,0 @@
-module Kernel
-  # A shortcut to define a toplevel concern, not within a module.
-  def concern(topic, &module_definition)
-    ::Object.concern topic, &module_definition
-  end
-end
